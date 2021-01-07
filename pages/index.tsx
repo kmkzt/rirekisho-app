832dedf4e7161c32269cf4e9465e7681e9fec14b
kmkzt <info.pscreator@gmail.com>
2021-01-07 12:29:11 +0900

    feat: Render sample.pdf

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,45 @@
+import {useCallback, useEffect, useRef, useState} from 'react'
 import Head from 'next/head'
 import * as pdfjsLib from 'pdfjs-dist'
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = require('pdfjs-dist/build/pdf.worker.entry')
 
-const loadingTask = pdfjsLib.getDocument('http://localhost:3000/sample.pdf')
-
-loadingTask.promise.then((pdf) => {
-  console.log(pdf)
-})
-
-export const Home = (): JSX.Element => (
+export const Home = (): JSX.Element => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [isLoading, setLoading] = useState(true)
+  const pdfRenderToCanvas = useCallback((pdf: pdfjsLib.PDFDocumentProxy) => {
+    pdf.getPage(1).then((page) => {
+      if (!canvasRef.current) return 
+      const canvasContext = canvasRef.current.getContext('2d')
+      if (!canvasContext) return
+      const renderTask = page.render({
+        canvasContext,
+        viewport: page.getViewport({ scale: 1.5 })
+      })
+      renderTask.promise.then(function () {
+        console.log('Page rendered');
+      });
+    })
+  },[])
+  useEffect(() => {
+    if (canvasRef.current && isLoading) {
+      setLoading(false)
+      const loadingTask = pdfjsLib.getDocument('http://localhost:3000/sample.pdf')
+      loadingTask.promise.then((pdf) => {
+        pdfRenderToCanvas(pdf)
+      })
+    }
+  }, [isLoading, setLoading])
+  return (
   <div className="container">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
-    <main></main>
+    {isLoading && <div>Loading ...</div>}
+    <canvas ref={canvasRef} />
   </div>
 )
+}
 
 export default Home

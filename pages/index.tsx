7c01029150ae1a49ddb8278df0aa5b929f3d44b2
kmkzt <info.pscreator@gmail.com>
2021-01-14 12:27:57 +0900

    feat: Render pdf from html

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 import { jsPDF } from 'jspdf'
 import { PdfViewer } from '../components/PdfViewer'
 
-// TODO: Fix for render "Hello world!"
-const doc = new jsPDF()
-doc.html('<h1>hello world!</h1>')
-const samplePdfData = doc.output()
-
 export const Home = (): JSX.Element => {
-  const [data] = useState<string>(samplePdfData)
-  // const [data] = useState<string>(samplePdfUrl)
+  const [html] = useState('<h1>hello world!</h1>')
+  const [pdfBlob, setPdfBlob] = useState<string>('')
+  // const [isLoading, setLoading] = useState(true)
+  const doc = useRef(new jsPDF())
+  const updatePdf = useCallback(async (htmlString: string) => {
+    await doc.current.html(htmlString, { jsPDF: doc.current })
+    // console.log(worker)
+    setPdfBlob(doc.current.output())
+  }, [])
+  useEffect(() => {
+    console.log('load: ', Date.now())
+    updatePdf(html)
+  }, [])
   return (
     <>
       <Head>
@@ -18,10 +24,12 @@ export const Home = (): JSX.Element => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="container">
-        <PdfViewer data={data} />
+        <button onClick={() => console.log(pdfBlob)}>Debug console.</button>
+        <button onClick={() => doc.current.save(Date.now() + '.pdf')}>
+          Download pdf
+        </button>
+        <PdfViewer data={pdfBlob} />
       </div>
-      <button onClick={() => console.log(samplePdfData)}>console.</button>
-      <button onClick={() => doc.save('a.pdf')}>Download pdf!</button>
     </>
   )
 }

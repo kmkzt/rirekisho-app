fd8eda5b7b0248ae381bcb6a0b90893ccb458c3c
kmkzt <info.pscreator@gmail.com>
2021-01-19 16:39:29 +0900

    chore: Add koruri font

--- mocks/htmlExampleData.ts
+++ mocks/htmlExampleData.ts
@@ -1,7 +1,59 @@
 import { HTMLFontFace } from 'jspdf'
 
+// TODO: Fix hostname
 // Refferences: https://github.com/MrRio/jsPDF/pull/3040/files#diff-539eefab6f8ab52ca4b421fe2d8964bdaf77aa47ac8146edb374af84eaaee46d
 export const exampleFontFaces: HTMLFontFace[] = [
+  {
+    family: 'Koruri',
+    weight: 300,
+    src: [
+      {
+        url: 'http://localhost:3000/koruri/Koruri-Light.ttf',
+        format: 'truetype',
+      },
+    ],
+  },
+  {
+    family: 'Koruri',
+    weight: 400,
+    src: [
+      {
+        url: 'http://localhost:3000/koruri/Koruri-Regular.ttf',
+        format: 'truetype',
+      },
+    ],
+  },
+  {
+    family: 'Koruri',
+    weight: 500,
+    src: [
+      {
+        url: 'http://localhost:3000/koruri/Koruri-Semibold.ttf',
+        format: 'truetype',
+      },
+    ],
+  },
+  {
+    family: 'Koruri',
+    weight: 700,
+    src: [
+      {
+        url: 'http://localhost:3000/koruri/Koruri-Bold.ttf',
+        format: 'truetype',
+      },
+    ],
+  },
+  {
+    family: 'Koruri',
+    weight: 900,
+    src: [
+      {
+        url: 'http://localhost:3000/koruri/Koruri-Extrabold.ttf',
+        format: 'truetype',
+      },
+    ],
+  },
+
   {
     family: 'Mouhitsu',
     src: [
@@ -58,7 +110,9 @@ export const exampleCss = `
 body {
   font-size: 14px;
 }
-
+.koruri {
+  font-family: 'Koruri';
+}
 .sans-serif {
   font-family: sans-serif;
 }
@@ -87,6 +141,7 @@ body {
 
 export const exampleHtml = `
 <div style="width: 200px; height: 200px;"> 
+  <p class="koruri">こるり koruri</p>
   <p class="default">
   The quick brown fox jumps over the lazy dog (default)
   <p>
